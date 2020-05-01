13d8f8c3bc01276e279953d553186330d8be2a12
kosst-p <pirozhkovks@yandex.ru>
2020-05-01 19:49:17 +0300

    created cards infected, recovered, deaths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import "./App.scss";
 import { Cards, Chart, CountryPicker } from "./components/imports.js";
+import { fetchData } from "./api/index.js";
 
 export default class App extends Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+    console.log(this.state.data);
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <div className="container">
-        <Cards />
+        <Cards data={data} />
         <Chart />
         <CountryPicker />
       </div>
