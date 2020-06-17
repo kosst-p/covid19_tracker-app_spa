0656f8036f222ada7c97d4e1541abb1200dc782d
kosst-p <pirozhkovks@yandex.ru>
2020-06-17 20:32:09 +0300

    Updates

---
+++ precache-manifest.bdef80f7d4636fd091749ff5736a11b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88740ade8585f0aef3968c51828329b4",
+    "url": "/covid19_tracker-app_spa/index.html"
+  },
+  {
+    "revision": "d6e362f63a5d70111d0a",
+    "url": "/covid19_tracker-app_spa/static/css/main.0b5ad1d2.chunk.css"
+  },
+  {
+    "revision": "45d955007492f5b8d5ae",
+    "url": "/covid19_tracker-app_spa/static/js/2.733e1333.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19_tracker-app_spa/static/js/2.733e1333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e362f63a5d70111d0a",
+    "url": "/covid19_tracker-app_spa/static/js/main.e632b342.chunk.js"
+  },
+  {
+    "revision": "d94730473a547a357d4b",
+    "url": "/covid19_tracker-app_spa/static/js/runtime-main.39a27164.js"
+  },
+  {
+    "revision": "a1e3ebeb24c584c4dedfa9d1bfc0141b",
+    "url": "/covid19_tracker-app_spa/static/media/stay-home.a1e3ebeb.png"
+  }
+]);
