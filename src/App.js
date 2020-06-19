2d2e9d286adfa7e9d40467363dc7dc70d01b5735
kosst-p <pirozhkovks@yandex.ru>
2020-06-19 16:56:06 +0300

    upd_v2 settings ym

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import classes from "./App.module.scss";
 import { Cards, Chart, CountryPicker } from "./components/imports.js";
 import { fetchData } from "./api/index.js";
 import coronaImage from "./images/stay-home.png";
-import { YMInitializer } from "react-yandex-metrika";
 
 export default class App extends Component {
   state = {
@@ -35,11 +34,6 @@ export default class App extends Component {
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
-        <YMInitializer
-          accounts={[65008369]}
-          options={{ webvisor: true }}
-          version="2"
-        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
+import { YMInitializer } from "react-yandex-metrika";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <>
+    <YMInitializer
+      accounts={[65008369]}
+      options={{ webvisor: true }}
+      version="2"
+    />
+    <App />
+  </>,
+  document.querySelector("#root")
+);
